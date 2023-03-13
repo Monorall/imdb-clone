f28dc3cb1355c286cc88b6283a484701e3535049
Monoral <goldflesher@gmail.com>
2023-03-13 12:07:42 +0600

    Updated controller for user login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back, #{user.name}!"
@@ -14,5 +14,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to movies_url, status: :see_other,
+                notice: "You're now signed out!"
   end
 end
