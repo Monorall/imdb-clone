41e428683635f5af73dfc3c1c9bf8882be28cb91
Monoral <goldflesher@gmail.com>
2023-03-13 18:03:21 +0600

    Add admin use. Made it possible to add, delete and edit movies only to the admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
+  end
 
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
 
 
   private
@@ -21,5 +29,5 @@ class ApplicationController < ActionController::Base
     current_user == user
   end
 
-  helper_method :current_user?
+  helper_method :current_user_admin?
 end
